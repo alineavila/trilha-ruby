5ccd39d0f09d0e4f2d77c5f0a7f69ff93c262bbd
alineavila <aline.avila01@gmail.com>
2020-09-01 23:18:18 -0300

    finalizado aula 2

--- Trilha Ruby on Rails/maior_ou_menor.rb
+++ Trilha Ruby on Rails/maior_ou_menor.rb
@@ -6,12 +6,22 @@ puts "Começaremos o jogo para você, " + nome + "\n"
 puts "Escolhendo um número entre 0 e 200"
 numero_secreto = 175
 puts "Escolhido... que tal adivinhar hoje nosso número secreto?" + "\n\n"
-puts "Tentativa 1"
-puts "Entre com o número"
-chute = gets
-puts "Será que acertou? Você chutou " + chute
-if  numero_secreto == chute.to_i
-    puts "Acertou!"
-else
-    puts "Errou"
+limiteDeTentativas = 3
+for tentativa in 1..limiteDeTentativas
+    puts "Tentativa " + tentativa.to_s + " de " + limiteDeTentativas.to_s
+    puts "Entre com o número"
+    chute = gets
+    puts "Será que acertou? Você chutou " + chute
+    acertou = numero_secreto == chute.to_i
+    if acertou
+        puts "Acertou!"
+        break
+    else
+        maior = numero_secreto > chute.to_i
+        if maior 
+            puts "O número secreto é maior!"
+        else 
+            puts "O número secreto é menor!"
+        end
+    end
 end
