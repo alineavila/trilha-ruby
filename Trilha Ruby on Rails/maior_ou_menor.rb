b38c74d6959bddcc5e2f425a27598530e18092d0
alineavila <aline.avila01@gmail.com>
2020-09-01 22:48:55 -0300

    código refatorado

--- Trilha Ruby on Rails/maior_ou_menor.rb
+++ Trilha Ruby on Rails/maior_ou_menor.rb
@@ -1,18 +1,17 @@
 puts "Bem-vindo ao jogo da adivinhação"
 puts "Qual é o seu nome?"
 nome = gets
-puts
-puts
-puts
-puts "Começaremos o jogo para você, " + nome
-puts
+puts "\n\n\n"
+puts "Começaremos o jogo para você, " + nome + "\n"
 puts "Escolhendo um número entre 0 e 200"
 numero_secreto = 175
-puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
-puts
-puts
+puts "Escolhido... que tal adivinhar hoje nosso número secreto?" + "\n\n"
 puts "Tentativa 1"
 puts "Entre com o número"
 chute = gets
 puts "Será que acertou? Você chutou " + chute
-puts numero_secreto == chute.to_i
+if  numero_secreto == chute.to_i
+    puts "Acertou!"
+else
+    puts "Errou"
+end
