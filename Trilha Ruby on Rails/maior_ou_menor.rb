784df52fb6e2f47b8998ea706ce94a94c925a5bf
alineavila <aline.avila01@gmail.com>
2020-09-04 16:35:24 -0300

    encapsulado em métodos

--- Trilha Ruby on Rails/maior_ou_menor.rb
+++ Trilha Ruby on Rails/maior_ou_menor.rb
@@ -1,11 +1,20 @@
-puts "Bem-vindo ao jogo da adivinhação"
-puts "Qual é o seu nome?"
-nome = gets
-puts "\n\n\n"
-puts "Começaremos o jogo para você, " + nome + "\n"
-puts "Escolhendo um número entre 0 e 200"
-numero_secreto = 175
-puts "Escolhido... que tal adivinhar hoje nosso número secreto?" + "\n\n"
+def da_boas_vindas
+    puts "Bem-vindo ao jogo da adivinhação"
+    puts "Qual é o seu nome?"
+    nome = gets
+    puts "\n\n\n"
+    puts "Começaremos o jogo para você, " + nome + "\n"
+end 
+def sorteia_numero_secreto
+    puts "Escolhendo um número entre 0 e 200"
+    sorteado = 175
+    puts "Escolhido... que tal adivinhar hoje nosso número secreto?" + "\n\n" 
+    return sorteado   
+end
+
+da_boas_vindas
+numero_secreto = sorteia_numero_secreto
+
 limiteDeTentativas = 3
 for tentativa in 1..limiteDeTentativas
     puts "Tentativa " + tentativa.to_s + " de " + limiteDeTentativas.to_s
